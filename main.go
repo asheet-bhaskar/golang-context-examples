f37203fd2deb25de64e858795b40cca2714ecce4
Asheet Kumar <asheet.kumar@go-jek.com>
2019-10-20 23:17:31 +0530

    add a context which will be canceled at last

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func operaionOne(ctx context.Context) {
+	n := 1
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("context canceled for op-1")
+			return // returning not to leak the goroutine
+		default:
+			fmt.Printf("OperationOne: %d\n", n)
+			time.Sleep(500 * time.Millisecond)
+			n++
+		}
+	}
+}
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	operaionOne(ctx)
+}
