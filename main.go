63fae8caa347580ed2ad9484afe2fd5675861523
Asheet Kumar <asheet.kumar@go-jek.com>
2019-10-20 23:41:33 +0530

    uses drived contexts for two operations separately

--- main.go
+++ main.go
@@ -21,9 +21,31 @@ func operaionOne(ctx context.Context) {
 	}
 }
 
+func operaionTwo(ctx context.Context) {
+	n := 1
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("context canceled for op-2")
+			return // returning not to leak the goroutine
+		default:
+			fmt.Printf("OperationTwo: %d\n", n)
+			time.Sleep(250 * time.Millisecond)
+			n++
+		}
+	}
+}
+
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+	d := time.Now().Add(5000 * time.Millisecond)
+	ctx, _ = context.WithDeadline(context.Background(), d)
 	go operaionOne(ctx)
-	time.Sleep(5 * time.Second)
+
+	d = time.Now().Add(10000 * time.Millisecond)
+	ctx, _ = context.WithDeadline(context.Background(), d)
+	go operaionTwo(ctx)
+
+	time.Sleep(20 * time.Second)
 }
